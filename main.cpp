a85fe8ff234f86936649624121c6816c494e1ca0
Gantelet Mathis <devecout@gmail.com>
2020-11-12 20:04:18 +0100

    Main avec affichage des Roads pour [DEBUG]

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <SFML/graphics.hpp>
 #include <SFML/System.hpp>
+#include "Road.h"
 
 /*
 			ENUMERATION :
@@ -9,8 +10,6 @@
 	L'avantage de l'enumeration est d'avoir une liste d'identifiant
 	qui par définitions ne possède pas de doublons. De plus les identfiant
 	qui vont remplacé les enumerations sont créée automatiquement (0,1,2,3,...)
-	
-
 */
 
 enum e_id_gamemode {
@@ -22,31 +21,38 @@ enum e_id_gamemode {
 
 int main()
 {
-	sf::RenderWindow rWindow(sf::VideoMode(900,900),"Tower Defence");
+	sf::RenderWindow rWindow(sf::VideoMode(900,900),"Firewall Defense");
 	sf::Event event;
-	sf::View view;
-	// Salut, c'est le debut du jeu
-	int gameMode = MENU;
+	sf::View view(sf::Vector2f(450,450),sf::Vector2f(900,900));
+	int gameMode = INGAME;
+	std::vector<Road> pathMap1;
+
+	//INITIALISATION PATH 1
+
+	Road::createMap(pathMap1, 1);
 
 	while (rWindow.isOpen())
 	{
-
 		//----------Modification de la taille de la vue pour ne pas "étirer" l'affichage
+	
 		while (rWindow.pollEvent(event))
 		{
 			if (event.type == sf::Event::Resized)
 			{
-				if (rWindow.getSize().x > rWindow.getSize().y)//redimensionnement par ratio
+				if (rWindow.getSize().x < rWindow.getSize().y)//redimensionnement par ratio
 				{
-					view.setSize(sf::Vector2f(view.getSize().x, view.getSize().x * rWindow.getSize().y / rWindow.getSize().x));
+					view.setSize(sf::Vector2f(900, view.getSize().x * rWindow.getSize().y / rWindow.getSize().x));
 				}
 				else
 				{
-					view.setSize(sf::Vector2f(view.getSize().y * rWindow.getSize().x / rWindow.getSize().y, view.getSize().y));
+
+					view.setSize(sf::Vector2f(view.getSize().y * rWindow.getSize().x / rWindow.getSize().y, 900));
 				}
 			}
+		}if (event.type == sf::Event::Closed)
+		{
+			rWindow.close();
 		}
-		sf::Mouse::getPosition();
 
 		//------------------------------------------CHOIX MENU-------------------------------------------
 		switch (gameMode)
@@ -54,20 +60,38 @@ int main()
 		case MENU:
 
 			//--------------REFRESH MENU----------------------
-			//menu.refresh(rWindow,       );
+			//menu.refresh(rWindow,);
+			
+
 
 
 			break;
 		case INGAME:
 			
 			//--------------REFRESH MENU----------------------
+			sf::RectangleShape r;
+			for (int y = 0; y < 20; y++)	//DEBUG BackGround
+			{
+				for (int x = 0; x < 20; x++)
+				{
+					r.setSize(sf::Vector2f(45, 45));
+					r.setPosition(x * 45, y * 45);
+					r.setFillColor(sf::Color(70,70,70));
+					rWindow.draw(r);
+				}
+			}
 
 
+			for (int i = 0; i < pathMap1.size(); i++)
+			{
+				pathMap1[i].beDraw(rWindow);
+			}
 
 			break;
 		}
-
-
+		rWindow.setView(view);
+		rWindow.display();
+		rWindow.clear();
 	}
 	return 0;
 }
