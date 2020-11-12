009f1db7ef3457ecdb513ff27ed28019f0e06456
Sylvain Churlet <churlet.sylvain@gmail.com>
2020-11-12 23:39:57 +0100

    Ajout de button.cpp + header

---
+++ header/const.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#define SCREEN_WIDTH 900
+#define SCREEN_HEIGHT 900
+
+#define LARGEUR_RECT 200
+#define HAUTEUR_RECT 70
+
+#define BUTTON_SIZE sf::Vector2f(200, 70)
+#define BUTTON_POS_PLAY sf::Vector2f(120, 120)
+#define BUTTON_POS_OPT sf::Vector2f(300,300)
+#define BUTTON_POS_RGL sf::Vector2f(600,600)
+#define BUTTON_POS_EXT sf::Vector2f(0,0)
---
+++ header/menu.h
@@ -0,0 +1,6 @@
+#pragma once
+
+
+void beDraw(sf::RenderWindow& window, sf::Vector2i& posSouris);
+
+bool isButtonSelect(sf::RenderWindow& window, sf::Vector2i& posSouris, sf::Vector2f posButton);
